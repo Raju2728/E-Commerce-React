03ba3c4adf0852ad93065d0f16ff307b1a23351b
Rajesh2730 <codemaker2k4@gmail.com>
2026-04-27 16:12:37 +0530

    Change the folder name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter} from 'react-router-dom';
-import App from './Components/App/App.js';
+import App from './components/app/App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
